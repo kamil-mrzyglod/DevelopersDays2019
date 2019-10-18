a0156a5d1e2c974974a17dc680867f840c14e43d
Kamil Mrzygłód <kamil@thecloudtheory.com>
2019-10-18 12:27:32 +0200

    First commit

---
+++ DevelopersDays.Front/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevelopersDays.Front
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Console World!");
+
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            using (var http = new HttpClient())
+            {
+                while (true)
+                {
+                    var response = await http.GetAsync("http://10.0.145.163");
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(result);
+
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+    }
+}
