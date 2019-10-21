59289a81a3536863b7542efac0b8cc9c5ce2cac4
Sławomir Kwasiborski <skwa@demant-technology.com>
2019-10-21 19:43:42 +0200

    better kubectl debuging script is now used to find pod name

---
+++ DevelopersDays.Web/kubectl-selector-exec.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# based on script by Pavel Agarkov - https://itnext.io/debugging-asp-net-core-in-kubernetes-from-vscode-294a728031e6
+
+set -e
+# echo "processing params"
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --namespace)
+            NAMESPACE=$2
+            shift 2
+            ;;
+        -s | --selector)
+            SELECTOR=$2
+            shift 2
+            ;;
+        *)
+            PARAMS="$PARAMS$1 "
+            shift 1
+            ;;
+    esac
+done
+
+#echo "serching for pod in ${NAMESPACE:-default} namespace and with $SELECTOR"
+POD=`kubectl get pods -n ${NAMESPACE:-default} --selector=$SELECTOR -o jsonpath='{.items[0].metadata.name}'`;
+# echo "starting debugger on $POD";
+kubectl exec $POD -i -- $PARAMS;
